a27aac5fdea3d614447008c37eaaa7bcc100938b
Iryna Bilyk <bilykirochka@gmail.com>
2023-10-19 14:31:41 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');
+ const instance = basicLightbox.create(`
+    <img src="evt.target.dataset.source" width="800" height="600">
+`)
+
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+
+galleryList.addEventListener('click', onClick);
+
+
+function onClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    // console.log(evt.target.dataset.source);
+    console.dir(evt.target.src);
+    const instance = basicLightbox.create(`
+    <img src="evt.target.src" width="800" height="600">
+`)
+    
+   instance.show();
+   
+ 
+}
+
